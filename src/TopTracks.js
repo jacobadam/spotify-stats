e068b25ac7520d8ae0eab956ca0b3421fdc56e81
jacobadam <jacobanevitt@gmail.com>
2024-05-02 14:26:59 -0700

    refactor topTracks data into a table and style

--- src/TopTracks.js
+++ src/TopTracks.js
@@ -1,17 +1,29 @@
 import React from "react";
+import "./TopTracks.css";
 
 export default function TopTracks({ topTracks }) {
   return (
     <div id="topTracks" className="tabcontent">
-      <h3>Top Tracks</h3>
+      <h3>Top Tracks (last 6 months)</h3>
       {topTracks.length > 0 && (
-        <ul>
-          {topTracks.map((topTrack) => (
-            <div key={topTrack.id}>
-              {topTrack.artists[0].name} - {topTrack.name}
-            </div>
+        <table>
+          {topTracks.map((topTrack, i) => (
+            <tr className="artistRow" key={topTrack.id}>
+              <td>{i + 1}</td>
+              <td className="artistImage">
+                <img
+                  alt="artist"
+                  width="50px"
+                  src={topTrack.album.images[0].url}
+                />
+              </td>
+              <div className="topTrackDetail">
+                <td className="topTrackArtist">{topTrack.name}</td>
+                <td className="topTrackTitle">{topTrack.artists[0].name}</td>
+              </div>
+            </tr>
           ))}
-        </ul>
+        </table>
       )}
     </div>
   );
