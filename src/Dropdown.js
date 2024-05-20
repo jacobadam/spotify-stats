7f2a6642de3b37a3112d50f172b81c221791c265
jacobadam <jacobanevitt@gmail.com>
2024-05-20 12:45:49 -0700

    add logo for account dropdown, set timeout for drop down and styling

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,17 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./Dropdown.css";
+import downArrow from "./down-arrow.png";
+import upArrow from "./up-arrow.png";
 
 const AccountDropdown = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const timeoutRef = useRef(null);
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleMouseEnter = () => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  };
+
+  const handleMouseLeave = () => {
+    timeoutRef.current = setTimeout(() => {
+      setIsOpen(false);
+    }, 300);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <div className="account-dropdown">
+    <div
+      className="account-dropdown"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <button onClick={toggleDropdown} className="account-button">
         Account
+        <img
+          src={isOpen ? upArrow : downArrow}
+          alt={isOpen ? "up arrow" : "down arrow"}
+          className="arrow-icon"
+        />
       </button>
       {isOpen && (
         <div className="dropdown-menu">
