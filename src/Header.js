dc9b1211a2e96fa0b49d38f0fa98bc6b42a627d5
jacobadam <jacobanevitt@gmail.com>
2024-05-23 10:21:25 -0700

    add navigation to homepage for spotify logo in header

--- src/Header.js
+++ src/Header.js
@@ -3,13 +3,25 @@ import React from "react";
 import Dropdown from "./Dropdown";
 import spotifylogo from "./spotifylogo.png";
 import { useAuth } from "./Authentication";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
   const { loggedIn } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate("/");
+  };
 
   return (
     <header className="appHeader">
-      <img className="spotifyLogo" src={spotifylogo} alt="Spotify Logo" />
+      <img
+        className="spotifyLogo"
+        src={spotifylogo}
+        alt="Spotify Logo"
+        onClick={handleLogoClick}
+        style={{ cursor: "pointer" }}
+      />
       {loggedIn && (
         <div className="dropdownContainer">
           <Dropdown />
